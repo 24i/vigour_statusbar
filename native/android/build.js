d00a67e8269d69d1c884d32730583ecfbb777557
Michiel van Liempt <michiel.v.liempt@gmail.com>
2015-10-12 13:35:36 +0200

    renamed android project dir

--- native/android/build.js
+++ native/android/build.js
@@ -5,7 +5,7 @@ var Promise = require('promise')
 var path = require('path')
 
 // paths
-var srcPath = path.join(__dirname, 'src')
+var srcPath = path.join(__dirname, 'statusbar')
 
 log.info('building .aar files from source')
 
