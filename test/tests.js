026c005a8bafd9d8e7634391e6c05ffefcc12b53
ninjatux <valerio.barrila@gmail.com>
2015-12-18 11:12:50 +0100

    added more tests for double check functionalities in iOS

--- test/tests.js
+++ test/tests.js
@@ -26,16 +26,16 @@ module.exports = function (inject, type) {
     })
   })
 
-  it('should be able to change just color for background', function (done) {
+  it('should be able to set color for background to 000000', function (done) {
     this.timeout(5000)
     sb.background.color.on('data', (data) => {
-      expect(sb.background.color.val).to.equal('FF0000')
+      expect(sb.background.color.val).to.equal('000000')
       if (!manual) {
         done()
       }
     })
 
-    sb.background.color.val = 'FF0000'
+    sb.background.color.val = '000000'
 
     if (manual) {
       alert('check if the color of status bar changed')
@@ -45,15 +45,34 @@ module.exports = function (inject, type) {
     }
   })
 
-  it('should be able to change just the opacity for background', function (done) {
+  it('should be able to set color for background to ffffff', function (done) {
     this.timeout(5000)
-    sb.background.opacity.on('data', (data) => {
-      expect(sb.background.opacity.val).to.equal(0.9)
+    sb.background.color.on('data', (data) => {
+      expect(sb.background.color.val).to.equal('ffffff')
       if (!manual) {
         done()
       }
     })
-    sb.background.opacity.val = 0.9
+
+    sb.background.color.val = 'ffffff'
+
+    if (manual) {
+      alert('check if the color of status bar changed')
+      setTimeout(function () {
+        done()
+      }, 1000)
+    }
+  })
+
+  it('should be able to set the opacity for background to 0', function (done) {
+    this.timeout(5000)
+    sb.background.opacity.on('data', (data) => {
+      expect(sb.background.opacity.val).to.equal(0)
+      if (!manual) {
+        done()
+      }
+    })
+    sb.background.opacity.val = 0
 
     if (manual) {
       alert('check if the opacity of status bar changed')
@@ -63,15 +82,33 @@ module.exports = function (inject, type) {
     }
   })
 
-  it('should be able to change just the color for text', function (done) {
+  it('should be able to set the opacity for background to 1', function (done) {
     this.timeout(5000)
-    sb.text.color.on('data', (data) => {
-      expect(sb.text.color.val).to.equal('00ff00')
+    sb.background.opacity.on('data', (data) => {
+      expect(sb.background.opacity.val).to.equal(1)
       if (!manual) {
         done()
       }
     })
-    sb.text.color.val = '00ff00'
+    sb.background.opacity.val = 1
+
+    if (manual) {
+      alert('check if the opacity of status bar changed')
+      setTimeout(function () {
+        done()
+      }, 1000)
+    }
+  })
+
+  it('should be able to set the color for text to 000000', function (done) {
+    this.timeout(5000)
+    sb.text.color.on('data', (data) => {
+      expect(sb.text.color.val).to.equal('000000')
+      if (!manual) {
+        done()
+      }
+    })
+    sb.text.color.val = '000000'
 
     if (manual) {
       alert('check if the color of text changed')
@@ -81,15 +118,51 @@ module.exports = function (inject, type) {
     }
   })
 
-  it('should be able to change just the opacity for text', function (done) {
+  it('should be able to set the color for text to ffffff', function (done) {
     this.timeout(5000)
-    sb.text.opacity.on('data', (data) => {
-      expect(sb.text.opacity.val).to.equal(0.9)
+    sb.text.color.on('data', (data) => {
+      expect(sb.text.color.val).to.equal('ffffff')
       if (!manual) {
         done()
       }
     })
-    sb.text.opacity.val = 0.9
+    sb.text.color.val = 'ffffff'
+
+    if (manual) {
+      alert('check if the color of text changed')
+      setTimeout(function () {
+        done()
+      }, 1000)
+    }
+  })
+
+  it('should be able to set the opacity for text to 1', function (done) {
+    this.timeout(5000)
+    sb.text.opacity.on('data', (data) => {
+      expect(sb.text.opacity.val).to.equal(1)
+      if (!manual) {
+        done()
+      }
+    })
+    sb.text.opacity.val = 1
+
+    if (manual) {
+      alert('check if the opacity of text changed')
+      setTimeout(function () {
+        done()
+      }, 2000)
+    }
+  })
+
+  it('should be able to set the opacity for text to 0', function (done) {
+    this.timeout(5000)
+    sb.text.opacity.on('data', (data) => {
+      expect(sb.text.opacity.val).to.equal(0)
+      if (!manual) {
+        done()
+      }
+    })
+    sb.text.opacity.val = 0
 
     if (manual) {
       alert('check if the opacity of text changed')
@@ -100,7 +173,7 @@ module.exports = function (inject, type) {
   })
 
   it('should be able to control the display of status bar', function (done) {
-    this.timeout(5000)
+    this.timeout(10000)
     var displayShould
     var times = 0
     sb.display.on('data', () => {
@@ -120,10 +193,10 @@ module.exports = function (inject, type) {
       }, 1000)
     }, 1000)
     if (manual) {
-      alert('did the statusbar changed 3 times (hidden, overlay, top)?')
+      alert('the statusbar changed 3 times (hidden, overlay, top)?')
       setTimeout(function () {
         done()
-      }, 1000)
+      }, 5000)
     }
   })
 }
