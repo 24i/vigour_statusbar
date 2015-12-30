da5cb4a33e63aa3bf19e82d7f910a91098fb0984
ninjatux <valerio.barrila@gmail.com>
2015-12-30 18:57:02 +0100

    more concise tests

--- test/tests.js
+++ test/tests.js
@@ -28,68 +28,51 @@ module.exports = function (inject, type) {
 
   it('should be able to set color for background to 000000', function (done) {
     this.timeout(5000)
-    sb.background.color.on('data', (data) => {
-      expect(sb.background.color.val).to.equal('000000')
-      if (!manual) {
-        done()
-      }
-    })
-
     sb.background.color.val = '000000'
-
     if (manual) {
       alert('check if the color of status bar changed')
       setTimeout(function () {
         done()
       }, 1000)
+    } else {
+      setTimeout(function () {
+        done()
+      })
     }
   })
 
   it('should be able to set color for background to ffffff', function (done) {
     this.timeout(5000)
-    sb.background.color.on('data', (data) => {
-      expect(sb.background.color.val).to.equal('ffffff')
-      if (!manual) {
-        done()
-      }
-    })
-
     sb.background.color.val = 'ffffff'
-
     if (manual) {
       alert('check if the color of status bar changed')
       setTimeout(function () {
         done()
       }, 1000)
+    } else {
+      setTimeout(function () {
+        done()
+      })
     }
   })
 
   it('should be able to set the opacity for background to 0', function (done) {
     this.timeout(5000)
-    sb.background.opacity.on('data', (data) => {
-      expect(sb.background.opacity.val).to.equal(0)
-      if (!manual) {
-        done()
-      }
-    })
     sb.background.opacity.val = 0
-
     if (manual) {
       alert('check if the opacity of status bar changed')
       setTimeout(function () {
         done()
       }, 1000)
+    } else {
+      setTimeout(function () {
+        done()
+      })
     }
   })
 
   it('should be able to set the opacity for background to 1', function (done) {
     this.timeout(5000)
-    sb.background.opacity.on('data', (data) => {
-      expect(sb.background.opacity.val).to.equal(1)
-      if (!manual) {
-        done()
-      }
-    })
     sb.background.opacity.val = 1
 
     if (manual) {
@@ -97,17 +80,15 @@ module.exports = function (inject, type) {
       setTimeout(function () {
         done()
       }, 1000)
+    } else {
+      setTimeout(function () {
+        done()
+      })
     }
   })
 
   it('should be able to set the color for text to 000000', function (done) {
     this.timeout(5000)
-    sb.text.color.on('data', (data) => {
-      expect(sb.text.color.val).to.equal('000000')
-      if (!manual) {
-        done()
-      }
-    })
     sb.text.color.val = '000000'
 
     if (manual) {
@@ -115,17 +96,15 @@ module.exports = function (inject, type) {
       setTimeout(function () {
         done()
       }, 1000)
+    } else {
+      setTimeout(function () {
+        done()
+      })
     }
   })
 
   it('should be able to set the color for text to ffffff', function (done) {
     this.timeout(5000)
-    sb.text.color.on('data', (data) => {
-      expect(sb.text.color.val).to.equal('ffffff')
-      if (!manual) {
-        done()
-      }
-    })
     sb.text.color.val = 'ffffff'
 
     if (manual) {
@@ -133,17 +112,15 @@ module.exports = function (inject, type) {
       setTimeout(function () {
         done()
       }, 1000)
+    } else {
+      setTimeout(function () {
+        done()
+      })
     }
   })
 
   it('should be able to set the opacity for text to 1', function (done) {
     this.timeout(5000)
-    sb.text.opacity.on('data', (data) => {
-      expect(sb.text.opacity.val).to.equal(1)
-      if (!manual) {
-        done()
-      }
-    })
     sb.text.opacity.val = 1
 
     if (manual) {
@@ -151,24 +128,25 @@ module.exports = function (inject, type) {
       setTimeout(function () {
         done()
       }, 2000)
+    } else {
+      setTimeout(function () {
+        done()
+      })
     }
   })
 
   it('should be able to set the opacity for text to 0', function (done) {
     this.timeout(5000)
-    sb.text.opacity.on('data', (data) => {
-      expect(sb.text.opacity.val).to.equal(0)
-      if (!manual) {
-        done()
-      }
-    })
     sb.text.opacity.val = 0
-
     if (manual) {
       alert('check if the opacity of text changed')
       setTimeout(function () {
         done()
       }, 2000)
+    } else {
+      setTimeout(function () {
+        done()
+      })
     }
   })
 
